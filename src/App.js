979d236db9c156f416c8a8ac35522e4ae8ea16f1
Pranay Aryal <drpranayaryal@gmail.com>
2018-09-17 00:52:51 -0400

    add some methods for fetching similar and for the same author

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './index.css'
 import Hero from './components/Hero';
 import Paper from './components/Paper';
 import PaperList from './components/PaperList';
@@ -31,6 +30,31 @@ class App extends Component {
           .catch(error => console.log(error))
       console.log('Data fetched!!')
   }
+
+  getSimilar = pid => {
+   
+    axios.get(`/api/${pid}`)
+          .then(response => {
+              //this.state.msg = response.data.msg
+              this.setState({msg: response.data.msg})
+              this.setState({papers: response.data.papers})
+          })
+          .catch(error => console.log(error))
+      console.log('Similar papers fetched!!')
+    
+  }
+
+  byAuthor = authorName => {
+      const search_url=`/api/search?q=${authorName.replace(/ /g, '+')}`
+      axios.get(search_url)
+            .then(response => {
+              this.setState({msg: response.data.msg})
+              this.setState({papers: response.data.papers})
+            })
+            .catch(error => console.log(error))
+        console.log('By authors fetched!!')
+
+  }
  
   render() {
     return (
@@ -38,7 +62,15 @@ class App extends Component {
         <Hero/>
         <br/>
         <div className="container">
-          <PaperList papers={this.state.papers}/>
+          <div className="columns">
+            <div className="column is-three-fourths">
+              <PaperList 
+              papers={this.state.papers}
+                handleGetSimilar={this.getSimilar}
+                byAuthor={this.byAuthor}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
