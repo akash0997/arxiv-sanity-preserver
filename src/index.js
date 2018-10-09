ae69cd09b7bfcc46dd5727cd7b93fe980c797f49
Pranay Aryal <drpranayaryal@gmail.com>
2018-10-08 23:13:49 -0400

    bulma import in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './App.scss';
 import registerServiceWorker from './registerServiceWorker';
-import 'bulma/css/bulma.min.css'
+// import 'bulma/css/bulma.min.css'
+// import 'bulma/bulma.sass'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
