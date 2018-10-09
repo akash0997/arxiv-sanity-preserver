5a55cd1d1ebbbcabe27205082c1cef5a14cd1d0e
Pranay Aryal <drpranayaryal@gmail.com>
2018-10-08 23:13:15 -0400

    add some login booleans in the Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,33 @@
 
 import React from 'react';
 
-const Card = props => {
+const Hero = props => {
         return (
             <section className="hero is-warning">
                 <div className="hero-body">
                     <div className="container">
-                        <h1 className="title">
-                            Arxiv Sanity Preserver
-                        </h1>
-                    <h2 className="subtitle">
-                        Built in spare time by <a href="https://twitter.com/karpathy">@karpathy</a> to accelerate research.<br/>
-                        Serving last 100 papers from cs.[CV|CL|LG|AI|NE]/stat.ML
-                        
-                    </h2>
+                    <div className="columns">
+                        <div className="column is-one-half">
+                            <h1 className="title">
+                                <a href="/">Arxiv Sanity Preserver</a>
+                            </h1>
+                            <h2 className="subtitle">
+                            Built in spare time by <a href="https://twitter.com/karpathy">@karpathy</a> to accelerate research.<br/>
+                            Serving last 100 papers from cs.[CV|CL|LG|AI|NE]/stat.ML
+                            
+                            </h2>
+                        </div>
+                        <div className="column is-one-third">
+                            <p>{!props.isLoggedIn ? '' : `Welcome, ${props.username}` }</p><br/>
+                            <p>{props.incorrectLogin? "Sorry, password doesn't match": '' }</p>
+                            <a className="button is-info" onClick={props.showLoginModal}>{props.isLoggedIn ? 'Logout': 'Login/Register'}</a>
+                        </div>
+                    </div>
+                    {/* <a className="button is-info" onClick={props.showLoginModal}>Login/Register</a> */}
                     </div>
                 </div>
             </section>
         );
 }
 
-export default Card
+export default Hero
